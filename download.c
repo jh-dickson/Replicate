a4a529dee5c57c9201225c84586e459e8599d8d9
jh-dickson <jhdickson@outlook.com>
2020-01-30 11:10:42 +0000

    Update download.c

--- download.c
+++ download.c
@@ -4,5 +4,5 @@ int main()
 {
     system("git clone https://github.com/jh-dickson/Replicate.git");
     system("cp Replicate/download.c download_out.c");
-    system("rm -r Replicate");
+    system("rm -rf Replicate");
 }
